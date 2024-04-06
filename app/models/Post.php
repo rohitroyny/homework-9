886ed0802272a8b9eaba3783723fd0fcf855fe8c
rohitroyny <157422306+rohitroyny@users.noreply.github.com>
2024-04-06 19:55:08 -0400

    Update Post.php

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,7 +2,34 @@
 
 namespace app\models;
 
+use app\core\Database; 
+
 class Post
 {
-    //todo make methods here
+    use Database; 
+
+    public function getAllPosts() {
+        $query = "select * from posts"; 
+        return $this->fetchAll($query); 
+    } 
+
+    public function getPostById($id) {
+        $query = "select * from posts where id = :id"; 
+        return $this->queryWithParams($query, ['id' => $id]); 
+    }
+
+    public function savePost($inputData) {
+        $query = "insert into posts (title, description) values (:title, :description);";
+        return $this->queryWithParams($query, $inputData); 
+    }
+
+    public function updatePost($inputData) {
+        $query = "update posts set title = :title, description= :description where id = :id"; 
+        return $this->queryWithParams($query, $inputData); 
+    }
+
+    public function deletePost($inputData) {
+        $query = "DELETE FROM posts where id = :id"; 
+        return $this->queryWithParams($query, $inputData); 
+    }
 }
